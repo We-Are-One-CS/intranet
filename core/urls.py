e04fb992dc691efb77958321e3ffe7905cea64f0
Matheus Elyasha <matheuselyasha@gmail.com>
2019-12-13 23:51:27 +0100

    Added different urls to organise views in the path

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.index, name='index'),
+    path('events/', views.EventsView.events, name='events'),
+    path('yearbook/', views.YearbookView.yearbook, name='yearbook'),
+    path('programs/', views.ProgramsView.programs, name='programs')
+
 ]
