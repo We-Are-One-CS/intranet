eb5c553ace1ea70cdd04ececfe9f4fbd55630853
PrissVI <46637324+PrissVI@users.noreply.github.com>
2020-03-18 13:54:45 +0100

    Update settings.py

--- intranet/settings.py
+++ intranet/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD':'Alilou02',
+        'PASSWORD':'',
         'HOST': 'localhost',  # set in docker-compose.yml
         'PORT': '',
     }
