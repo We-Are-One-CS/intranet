ae57ac83f7702ca82c126e9fe07134e367fb4a35
Matheus Elyasha <matheuselyasha@gmail.com>
2020-03-27 21:29:01 +0100

    Added Company model and refactored membership type

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Category, Structure, CotisationType, Event, SubjectImpact
+from .models import User, Category, Structure, MembershipType, Event, SubjectImpact, Company
 
 admin.site.site_header = 'WAO Dashboard'
 admin.site.site_title = 'WAO Admin'
@@ -9,10 +9,10 @@ admin.site.index_title = ''
 
 class MyUserAdmin(admin.ModelAdmin):
     list_display = (
-        'email', 'first_name', 'last_name', 'telephone', 'cotisation_paid', 'is_enterprise', 'is_superuser',
+        'email', 'first_name', 'last_name', 'telephone', 'membership_paid', 'company', 'is_superuser',
         'is_active')
-    list_filter = ('is_enterprise', 'is_superuser', 'cotisation_paid')
-    list_editable = ('is_superuser', 'is_enterprise', 'is_active', 'cotisation_paid')
+    list_filter = ('company', 'is_superuser', 'membership_paid')
+    list_editable = ('is_superuser', 'company', 'is_active', 'membership_paid')
     search_fields = ('last_name', 'first_name', 'email')
 
 
@@ -24,8 +24,9 @@ class EventAdmin(admin.ModelAdmin):
 admin.site.register(User, MyUserAdmin)
 admin.site.register(Category)
 admin.site.register(Structure)
-admin.site.register(CotisationType)
+admin.site.register(MembershipType)
 admin.site.register(SubjectImpact)
 admin.site.register(Event, EventAdmin)
+admin.site.register(Company)
 # admin.site.register(Yearbook) # Uncomment when Yearbook model is ready
 # admin.site.register(SelfDevelopmentProgram) # Uncomment when SelfDevelopmentProgram model is ready
