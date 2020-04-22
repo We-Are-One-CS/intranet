06247e8116e6f9eb0648c8b3d997ed320aefd264
Matheus Elyasha <matheuselyasha@gmail.com>
2020-04-22 21:09:46 +0200

    Added basic tests for project (add more please!)

---
+++ wao/tests/test_managers.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class TestCustomUserManager(TestCase):
+    def test_create_user(self):
+        self.fail()
+
+
+class TestCustomUserManager(TestCase):
+    def test_create_superuser(self):
+        self.fail()
