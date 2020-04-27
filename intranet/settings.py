d35a172e244f3d64cc81117af1c5a2028c228c76
Matheus Elyasha <matheuselyasha@gmail.com>
2020-04-28 00:30:30 +0200

    Added host for ec2

--- intranet/settings.py
+++ intranet/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ec2-176-34-97-213.eu-west-1.compute.amazonaws.com']
 
 # Authentification
 LOGIN_REDIRECT_URL = 'index'
