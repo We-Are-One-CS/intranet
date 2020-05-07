94f27fb3dee42b683514778cf6841604efd9044d
Matheus Elyasha <matheuselyasha@gmail.com>
2020-05-07 16:25:21 +0200

    STATIC_ROOT = 'static'

--- intranet/settings.py
+++ intranet/settings.py
@@ -158,6 +158,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_ROOT = 'static'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
